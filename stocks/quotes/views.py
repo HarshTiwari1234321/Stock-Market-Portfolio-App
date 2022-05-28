4a661308d83a8bc2a80147653d725edcd0fa2465
Harsh <harsh.tiwari.1242001@gmail.com>
2022-05-28 22:23:37 +0530

    commiting changes

---
+++ stocks/quotes/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
+
+def home(request):
+    import requests
+    import json
+
+    if request.method=='POST':
+        ticker = request.POST['ticker']
+        api_request=requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_fc37d60aa53147569ecdb88ccc44ec77")
+
+        try:
+            api=json.loads(api_request.content)
+        except Exception as e:
+            api="Error..."
+        return render(request,'home.html',{'api': api})
+
+    else:
+        return render(request,'home.html',{'ticker':"Enter a Ticker Symbol Above..."})
+
+
+
+
+
+
+
+
+def about(request):
+    return render(request,'about.html',{})
+
+
+def add_stock(request):
+    if request.method=='POST':
+        form=StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,("Stock Has Been Added!"))
+            return redirect('add_stock')
+
+    else:
+        ticker=Stock.objects.all()
+        return render(request,'add_stock.html',{'ticker': ticker})
